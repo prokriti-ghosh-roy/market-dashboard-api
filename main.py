585128c496f6d59f6053d0ad51ea7be232ff5501
prokriti-ghosh-roy <prokriti2001@gmail.com>
2026-03-18 19:10:44 +0530

    Update main.py
    fetch real -time AAPL price using yfinance

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello world")
+import yfinance as yf
+#choose a stock(e.g.,Apple)
+ticker = "AAPL"
+#Get the Data
+data = yf.Ticker(ticker)
+#print the current price
+print(f"The current price of {ticker} is: {data.info['currentPrice']}")
