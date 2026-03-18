9a7bbc1805bc2dac4b61af5fa8b3f71bf02ee48c
prokriti-ghosh-roy <prokriti2001@gmail.com>
2026-03-18 19:46:01 +0530

    Final project update
    Finalize README and fix variable name bugs.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # market-dashboard-api
 A project to track stock data
+# Market Dashboard Tool
+A Python-based CLI tool that fetches real-time stock and cryptocurrency data using the Yahoo Finance API.
+
+## Features
+* **Real-time Data:** Fetches live prices and currency information.
+* **Interactive Interface:** Allows users to query multiple stocks in one session.
+* **Error Handling:** Validates ticker symbols to prevent program crashes.
+
+## Tech Stack
+* **Language:** Python 3.x
+* **Libraries:** `yfinance`
+
+## How to Run
+1. Clone this repository.
+2. Install dependencies: `pip install yfinance`
+3. Run the script: `python main.py`
--- main.py
+++ main.py
@@ -5,4 +5,22 @@ import yfinance as yf
 # data = yf.Ticker(ticker_symbol)
 # #print the result
 # print(f"Current price of{ticker_symbol.upper()} is: {data.info['currentPrice']}")
+print("_ _ _Market Dashboard API_ _ _")
+print("Type 'exit' to stop the program")
+while True:
+  #ask the user for a stock symbol
+  user_input = input("\nEnter a stock symbol (e.g., TSLA, BTC-USD): ")
+  #check if the user wants to quit
+  if user_input.lower() == 'exit':
+    print("closing dashboard. Goodbye!")
+    break
+
+  try:
+    #get the data
+    data = yf.Ticker(user_input)
+    price = data.info['currentPrice']
+    currency = data.info['currency']
+    print(f"The current price of {user_input.upper()} is: {price} {currency}")
+  except Exception:
+    print("Could not find that symbol. Please try again (e.g., AAPL.)")
 
